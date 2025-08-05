fb17a64ffcf94f2e7f27d5e53d62dcb681dab645
haibinh.phan <haibinh.phan@shopee.com>
2025-08-05 18:04:06 +0700

    feat: Add PWA

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,6 +70,68 @@ const config: Config = {
     ],
   ],
 
+  plugins: [
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: false,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/frontend-system-design/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/frontend-system-design/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/frontend-system-design/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/frontend-system-design/img/docusaurus.svg",
+            color: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/frontend-system-design/img/docusaurus.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
+    ],
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
---
+++ src/pages/offline.md
@@ -0,0 +1,22 @@
+# You're Offline
+
+You're currently offline, but you can still browse the pages you've previously visited.
+
+## Available Content
+
+When you're online, this site caches all the pages you visit so you can access them later when offline.
+
+## What you can do:
+
+- Browse previously visited documentation pages
+- Read cached blog posts
+- Navigate through the sidebar (for cached content)
+
+## To get the latest content:
+
+- Connect to the internet
+- Refresh the page to sync with the latest updates
+
+---
+
+_This page is automatically shown when you try to access a page that hasn't been cached yet._
